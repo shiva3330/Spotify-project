37e904cd1439be308ed506fe8afa8ea4640584a7
shiva <shivapalakurthi69@gmail.com>
2025-08-26 22:45:47 +0530

    bug

--- logic.js
+++ logic.js
@@ -75,7 +75,7 @@ const playMusic = (track, pause = false) => {
 async function displayAlbums() {
   
   let section = document.querySelector(".section")
-  let a = await fetch("/songs/")
+  let a = await fetch("songs/")
   let response = await a.text()
   let div = document.createElement("div")
   div.innerHTML = response
@@ -85,7 +85,7 @@ async function displayAlbums() {
     const e = array[index]; 
     if (e.href.includes("/songs/")) {
       let folder = e.href.split("/").slice(-1)[0];
-      let a = await fetch(`/songs/${folder}/info.json`)
+      let a = await fetch(`songs/${folder}/info.json`)
       let response = await a.json()
       section.innerHTML = section.innerHTML + `<div data-folder="${folder}" class="album">
       <img src="songs/${folder}/cover.jpg" alt="" id="song-img" />
